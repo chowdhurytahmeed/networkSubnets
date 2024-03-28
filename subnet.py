f81e39ae6da7ed2c5270518c2f03ce87ae236df7
Tahmeed Chowdhury <31861714+chowdhurytahmeed@users.noreply.github.com>
2024-03-28 03:52:41 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 2990
+
+#problem 1
+p1Network = '130.87.81.0'
+p1Broadcast = '130.87.81.255'
+p1First = '130.87.81.1'
+p1Last = '130.87.81.254'
+
+#problem 2
+p2Network = '41.72.43.16'
+p2Broadcast = '41.72.43.31'
+p2First = '41.72.43.17'
+p2Last = '41.72.43.30'
+
+#problem 3
+p3Network = '208.57.4.0'
+p3Broadcast = '208.57.4.255'
+p3First = '208.57.4.1'
+p3Last = '208.57.4.254'
+
+#problem 4
+p4Network = '119.42.222.96'
+p4Broadcast = '119.42.222.111'
+p4First = '119.42.222.97'
+p4Last = '119.42.222.110'
+
+#problem 5
+p5Network = '30.27.184.128'
+p5Broadcast = '30.27.184.255'
+p5First = '30.27.184.129'
+p5Last = '30.27.184.254'
+
+#problem 6
+p6Network = '197.12.146.184'
+p6Broadcast = '197.12.146.191'
+p6First = '197.12.146.185'
+p6Last =  '197.12.146.190'
+
+#problem 7
+p7Network = '108.254.107.192'
+p7Broadcast = '108.254.107.255'
+p7First = '108.254.107.193'
+p7Last = '108.254.107.254'
+
+#problem 8
+p8Network = '19.239.69.0'
+p8Broadcast = '19.239.69.255'
+p8First = '19.239.69.1'
+p8Last = '19.239.69.254'
+
+#problem 9
+p9Network = '186.224.31.32'
+p9Broadcast = '186.224.31.63'
+p9First = '186.224.31.33'
+p9Last = '186.224.31.62'
+
+#problem 10
+p10Network = '97.209.249.0'
+p10Broadcast = '97.209.249.127'
+p10First = '97.209.249.1'
+p10Last = '97.209.249.126'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
